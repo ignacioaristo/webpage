c414ef58d332666d2877bf851b1385cc30d7dff8
Ignacio Aristo <ignacioaristo@gmail.com>
2021-02-22 13:04:23 -0300

    added link to webpage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+https://ignacioaristo.github.io/webpage/
+
 This web is on construction! Plase stand by.
---
+++ build/precache-manifest.ff2334c76aedfb98e2dfe5ff556d0abd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01933a3574f1aa188a107db15c6463b",
+    "url": "/webpage/index.html"
+  },
+  {
+    "revision": "d1f1ef563429e580a817",
+    "url": "/webpage/static/css/main.68c20fe1.chunk.css"
+  },
+  {
+    "revision": "ef3f33a412fd5c3a7132",
+    "url": "/webpage/static/js/2.40b4a298.chunk.js"
+  },
+  {
+    "revision": "40fca8087c4c22a986dedbda658a8ff4",
+    "url": "/webpage/static/js/2.40b4a298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f1ef563429e580a817",
+    "url": "/webpage/static/js/main.71c3e3db.chunk.js"
+  },
+  {
+    "revision": "586f282669875ebfe528",
+    "url": "/webpage/static/js/runtime-main.d0800523.js"
+  },
+  {
+    "revision": "f6294b21b79a190a7a1702ab29c5c838",
+    "url": "/webpage/static/media/Camiones.f6294b21.jpg"
+  },
+  {
+    "revision": "42487c8f2b391bd2fce1573a6714dd2d",
+    "url": "/webpage/static/media/Cargadora.42487c8f.jpg"
+  },
+  {
+    "revision": "7900b169303d917d2e71c734a15281ad",
+    "url": "/webpage/static/media/Compactador.7900b169.jpg"
+  },
+  {
+    "revision": "38517d57f5baa02ea2c73e5b96e1ceef",
+    "url": "/webpage/static/media/CompactadorAsfalto.38517d57.jpg"
+  },
+  {
+    "revision": "cb9ca0b6b26ddea7c509969e305531ea",
+    "url": "/webpage/static/media/ImagenAlquileres.cb9ca0b6.jpg"
+  },
+  {
+    "revision": "0a8a077d1cff8992632fc92b7a48b69d",
+    "url": "/webpage/static/media/Motoniveladora.0a8a077d.jpg"
+  },
+  {
+    "revision": "6242a6b72582d54aab713526d8c79d3d",
+    "url": "/webpage/static/media/Motor.6242a6b7.jpg"
+  },
+  {
+    "revision": "8237edf126badd46edfaf9b4e060ca15",
+    "url": "/webpage/static/media/PM.8237edf1.jpg"
+  },
+  {
+    "revision": "8884681d28b3b5b1a51d8fba32a4dba2",
+    "url": "/webpage/static/media/Palaretro.8884681d.jpg"
+  },
+  {
+    "revision": "1e8121395a19c183775fef82a448950f",
+    "url": "/webpage/static/media/Tendedoras.1e812139.jpg"
+  }
+]);
